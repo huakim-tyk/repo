31ecab166adf978a4ee116df225be7a85d2850a3
my.name <my.email>
2023-06-16 16:07:04 +0000

    update

---
+++ pacman/copy_func.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+copy(){
+  rsync --mkpath -aixHAX "$dir$1" "$dest${1:-$2}"
+}
+
+link(){
+ f="$dest${1:-$2}"
+ rm -Rfv "$f"
+ ln -sTfv "$dir$1" "$f"
+}
+
+cldir(){
+ f="$dest${1}"
+ rm -Rvf "$f"
+ mkdir -p "$f"
+}
+
+parse(){
+  while read line; do
+    $1 $line
+  done
+}
+
+parsefile(){
+    sed -n '/^[[:space:]]*#/!p' "$dir$1.list" 
+    sed -n '/^[[:space:]]*#/!p' "$dir$1.list" | parse "$2"
+}
