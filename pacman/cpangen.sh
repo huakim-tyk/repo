4f281c6ffccac8ee081d2cc711141750c7f29944
huakim <fijik19@gmail.com>
2023-06-17 14:27:27 +0000

    update

---
+++ pacman/cpangen.sh
@@ -0,0 +1,13 @@
+#/bin/bash
+
+dir="$(realpath $(dirname $0))/var/lib/apt/local/pool/perl/"
+mkdir "${dir}" -p
+
+(
+cd "${dir}.cpan/build"
+cd build
+for i in "${@}"
+do
+    DEB_BUILD_OPTIONS=nocheck HOME="${dir}" cpan2deb --recursive "${i}"
+done
+)
