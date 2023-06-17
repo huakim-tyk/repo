9e895a11c6c7db3e25710e31814daeadd7ae88e1
myname <my@email.com>
2023-06-17 13:51:23 +0000

    update

--- commit-extra.sh
+++ commit-extra.sh
@@ -1,16 +1,13 @@
 #!/bin/sh
 dir1="$(realpath $(dirname $0))/"
-dest1="${1:-${dir1}extra_backup}/"
-rm -R "${dest1}"
-
-. "${dir1}copy-extra.sh"
-
-
-ln -sTvf "$(realpath "${dir1}.git")" "${dest1}.git"
+dir="${dir1}pacman/"
+. "${dir}listfiles.sh"
 
 (
-cd "${dest1}"
-git add --all
+cd "${dir1}"
+for i in $listfiles; do
+    git add "$(realpath --relative-to=.. ${i})"
+done
 git commit -m update
 git push
 )
