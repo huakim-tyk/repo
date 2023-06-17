6076832d66eb1ea5dc30a09ebd263bce269b92a7
root <root@vm2324647.firstbyte.club>
2023-06-17 18:03:38 +0000

    update

--- pacman/ftprepo.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-dir="$(realpath $(dirname $0))/"
-
-perl "${dir}clonerepo.pl"
-
-echo "${dir}vsftpd.conf"
-
-/usr/sbin/vsftpd "${dir}vsftpd.conf" "-oanon_root=${dir}var/lib/apt/local/repo/" "${@}" 
